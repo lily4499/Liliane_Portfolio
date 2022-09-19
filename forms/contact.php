12cbf8211c722a12e7f4ecc2491ad376116eb768
lily4499 <konissil@gmail.com>
2022-09-18 20:19:08 -0400

    version2

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,5 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'konissili@gmail.com';
@@ -39,3 +34,16 @@
 
   echo $contact->send();
 ?>
+
+
+
+<!-- <?php
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $subject = 
+} 
+
+
+
+?> -->
