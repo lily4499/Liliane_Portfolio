a13deb25730a7e1c27650a6929de730708e223a1
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-06-12 21:36:44 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,21 @@ document.getElementById('downloadResumeBtn').addEventListener('click', function(
     // Get the resume content
     var resumeContent = document.getElementById('resume').innerHTML;
 
-    // Create a blob with the resume content
+    // Create a blob with the resume content and include the links to the CSS files
     var blob = new Blob([`
         <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Lili's Resume</title>
+            <title>My Resume</title>
+            <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+            <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+            <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+            <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+            <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+            <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
             <link rel="stylesheet" href="styles.css">
-            <link href="assets/css/style.css" rel="stylesheet">
         </head>
         <body>
             ${resumeContent}
