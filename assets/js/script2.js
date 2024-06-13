300d329d004dcc66d45886d32a34234455b3852a
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-06-12 22:55:24 -0400

    Update script2.js

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -9,7 +9,7 @@ document.getElementById('downloadResumeBtn2').addEventListener('click', function
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Resume</title>
+            <title>Lili'S Resume</title>
             <link href="assets/vendor/aos/aos.css" rel="stylesheet">
             <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
             <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
